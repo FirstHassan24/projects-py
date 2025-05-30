52d4b3fa9db6d6e206704326ae971ab9d4e8b3db
Akilou24 <61370309+FirstHassan24@users.noreply.github.com>
2025-05-30 12:42:11 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -12,21 +12,26 @@
 # I encourage you to make methods for each piece of logic! You could imagine methods for things like displaying the word before each guess, knowing if the user has won, or knowing if the guess is correct.
 
 possible_words = ['ungulate', 'lungfish', 'axolotl', 'blobfish']
-#store the guessed letters:
-guessed_letter= []
 print("Let's play Hangman!\n")
 while True:
 	print("Enter a letter")
 	guess = input('> ')
-	for word in possible_words:
-# 1. if they guess a letter loop over the  list and see if its in their, if it is make it appear as _ except for the letter that the user guessed:
-		for letter in word:
-			# print(letter)
-			if guess == letter:
-				guessed_letter += guess
-				# print(guess)
-        # ^this should not print their is no h in letter
-			else:
-				print("_")
-			if guess == 'quit':
+
+	if guess == 'quit':
 				break
+	# 1. creat a function that stores gussed letter if they are the same as  letters in possible_words
+	# 1. if they guess a letter loop over the  list and see if its in their, if it is make it appear in its location ,except for the letter that the user guessed they appear as _:
+def hiden_words(guess,words):
+	words = possible_words
+	guessed_letter=""
+
+	for word in words:
+		for letter in word:
+			print(letter)
+			if guess in letter:
+				#if what the user gussed is in letter then add it to guessed_letter
+					guessed_letter += guess
+
+				# print(guess)
+			else:
+				guessed_letter += "_"
