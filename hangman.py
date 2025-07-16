453168ac21c2705391dc4a5d762a0b5763821f0f
Akilou24 <61370309+FirstHassan24@users.noreply.github.com>
2025-07-16 12:58:04 -0400

    i actually managed to set it up

--- hangman.py
+++ hangman.py
@@ -5,9 +5,9 @@
 # - Before each guess, display these two things:
 #     1. the current word, using underscores for letters that haven't been correctly guessed yet, like this: "_ _ a _" (the third letter is "a")
 #     2. the list of incorrect guesses (in the order they were given), like this: "Guessed: a w b"
-# - On the seventh incorrect guess, the player loses the game (display a message and break out of the loop).
-# - On revealing the entire word, the player wins the game (display a message and break out of the loop).
-# - You should validate that the input is valid: must be one letter, a to z. Be sure to handle capital letters, but I'll leave how up to you. If the input is invalid, tell the player.
+# - 1 On the seventh incorrect guess, the player loses the game (display a message and break out of the loop).
+# - 2 On revealing the entire word, the player wins the game (display a message and break out of the loop).
+# - 3 You should validate that the input is valid: must be one letter, a to z. Be sure to handle capital letters, but I'll leave how up to you. If the input is invalid, tell the player.
 
 # I encourage you to make methods for each piece of logic! You could imagine methods for things like displaying the word before each guess, knowing if the user has won, or knowing if the guess is correct.
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
- # if guessed_letter == possible_word:# tracks how many words you guess correctly and compare it
-        #     print("congratulation you win")
-        #     break
+requests
